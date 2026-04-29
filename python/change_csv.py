dad3a3a5998f5a78a7c40ce732af790e01d64b2d
mazorofek <169063202+mazorofek@users.noreply.github.com>
2026-04-29 23:26:26 +0300

    add 1000 jihad tweets

--- python/change_csv.py
+++ python/change_csv.py
@@ -1,11 +1,11 @@
 import pandas as pd
 
 # Load the source CSV
-df = pd.read_csv('tweets_from_the_site.csv')
+df = pd.read_csv('tweets_from_the_site3.csv')
 
 # Filter by 'Salafi jihadi' and select only text and label columns
 # Note: Ensure 'text' and 'Final Decision' match your actual column headers
 filtered_data = df[df['Final Decision'] == 'Salafi jihadi'][['Text', 'Final Decision']]
 
 # Save to the destination CSV
-filtered_data.to_csv('destination.csv', index=False)
+filtered_data.to_csv('destination3.csv', index=False)
